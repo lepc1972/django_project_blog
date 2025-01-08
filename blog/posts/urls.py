1f313ccc74b60332620bb31c957d40f144709719
lepc1972 <githublepc@gmail.com>
2025-01-08 18:21:48 -0500

    more url n view fixes

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.helloworld)
+    path('home/', views.home)
     
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,5 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def helloworld(request):
-    return HttpResponse('Hello World')
+posts = [ 
+         
+         {"id":1,
+          "title":"First Post",
+          "author":"John Doe",
+          "body":"This is the first post"}
+         ,
+          
+           { "id":2,
+            "title":"Second Post",
+            "author":"Jane Doe",
+            "body":"This is the second post"}
+           ,
+            
+           { "id":3,
+            "title":"Third Post",
+            "author":"John Doe",
+            "body":"This is the third post"},
+        ]
+
+def home(request):
+    html = ""
+    for post in posts:
+        html += f"<h2>{post['title']}</h2>"
+        html += f"<p>{post['author']}</p>"
+        html += f"<p>{post['body']}</p>"
+        html += "<hr>"
+        
+    return HttpResponse(html)
